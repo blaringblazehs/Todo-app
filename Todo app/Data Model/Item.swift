837b2ba180bd9463f16b1302cfb218a125bd992c
Himanshu Soni <himanshusoni@himanshus-macbook-air.local>
2018-06-07 18:08:21 +0530

    Convert to MVC Design Pattern

---
+++ Todo app/Data Model/Item.swift
@@ -0,0 +1,13 @@
+//
+//  Item.swift
+//  Todo app
+//
+//  Created by Himanshu Soni on 07/06/18.
+//  Copyright © 2018 Himanshu Soni. All rights reserved.
+//
+
+import Foundation
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
